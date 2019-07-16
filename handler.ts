f99b1dbd3215dee39daefe1815f56d1895568697
Igor Soroka <stupbird@gmail.com>
2019-07-16 21:29:15 +0300

    added recast request

--- handler.ts
+++ handler.ts
@@ -3,12 +3,10 @@ import Translator from "./src/translator";
 
 export const send: Handler = async (event: any, context: Context, callback: Callback) => {
   console.log("Sending reply to bot lambda function started");
-  const translator: Translator = new Translator();
-  const text = await translator.translateMessage("I am from Russia", "en", "fi");
-  console.log(text);
+
   callback(null, {
     statusCode: 200,
     headers: "application/json",
-    body: text.TranslatedText
+    body: "success"
   });
 };
--- src/chatBotReply.ts
+++ src/chatBotReply.ts
@@ -1,1 +1,27 @@
-export default class ChatBotReply {}
+import AWS from "aws-sdk";
+import { Request } from "recastai";
+import ILambdaResponse from "./domain/ILambdaResponse";
+import Translator from "./translator";
+
+const RECAST_TOKEN = process.env.recast_token || "empty";
+
+export default class ChatBotReply {
+  private originalMessage: string;
+
+  constructor(originalMessage: string) {
+    this.originalMessage = originalMessage;
+  }
+
+  public async send(): Promise<ILambdaResponse> {
+    const translator: Translator = new Translator();
+    const translatedMessageObj = await translator.translateMessage(this.originalMessage, "auto", "en");
+    const originalMsgLanguage: string = translatedMessageObj.SourceLanguageCode;
+    const recastRequest = new Request(RECAST_TOKEN, originalMsgLanguage);
+    const analysedText = await recastRequest.analyseText(translatedMessageObj.TranslatedText);
+
+    return {
+      statusCode: 200,
+      body: "Succeeded"
+    };
+  }
+}
